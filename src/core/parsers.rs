bc03a7ee4955558f0bde209d6ae3d08f5194f68c
Nalysius <anthony.bocci@protonmail.com>
2022-05-17 14:55:03 +0200

    Fix the format of the timestamp
    The timestamp given to the touch command was invalid, the seconds were
    not separated from the minutes by a dot.

--- src/core/parsers.rs
+++ src/core/parsers.rs
@@ -6,12 +6,16 @@ use super::fs::FileStat;
 pub fn nix_timestamp_parser(
     timestamp_str: &str
 ) -> String {
-    timestamp_str[0..19]
+    let mut timestamp = timestamp_str
 	.to_string()
 	.replace("-", "")
 	.replace(":", "")
 	.replace(" ", "")
-	.replace("'", "")
+	.replace("'", "");
+    timestamp = timestamp[0..14].to_string();
+
+    //YYYYMMDDhhmm.ss
+    format!("{}{}{}", timestamp[0..12].to_string(), ".".to_string(), timestamp[12..14].to_string())
 }
 
 /// Offloads the required fields from `stat` to parse timestamps
@@ -24,14 +28,13 @@ pub fn nix_stat_parser(
     let mut ctime = String::new();
 
     // Each line contains one timestamp
-    let mut index = 0;
+    let mut index: usize = 0;
     for line in stream.lines() {
-	if index == 0 {
-            ctime = nix_timestamp_parser(line);
-	} else if index == 1 {
-	    atime = nix_timestamp_parser(line);	    
-	} else if index == 2 {
-	    mtime = nix_timestamp_parser(line);
+	match index {
+	    0 => ctime = nix_timestamp_parser(line),
+	    1 => atime = nix_timestamp_parser(line),
+	    2 => mtime = nix_timestamp_parser(line),
+	    _ => {}
 	}
 	index = index + 1;
     }
